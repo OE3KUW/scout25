798f5fa63318d6ebfad26ae00ff55dd17ea634a7
OEeKUW <wolfgang.kuran@htlstp.ac.at>
2025-07-07 18:20:47 +0200

    first steps

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/******************************************************************
+
+                        s c o u t 2 5
+
+                                                   қuran july 2025
+******************************************************************/
+
+#include <Arduino.h>
+
+void setup() 
+{
+    Serial.begin(115200);
+    sei();
+    printf("start!\n");
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
+
